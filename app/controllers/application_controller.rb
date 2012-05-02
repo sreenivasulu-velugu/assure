6810310947432a5ce5944cdd0c70735fd9b42a55
shyam mohan kanojia <shyammohankanojia@gmail.com>
2012-05-03 00:18:40 +0530

    fixes current user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,13 +17,17 @@ class ApplicationController < ActionController::Base
     end
     end
 
-     def current_user
-         @current_user ||= User.find(session[:user_id]) if session[:user_id]
-     end
+    # def current_user
+    #   begin
+    #     @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    #   rescue Mongoid::Errors::DocumentNotFound
+    #     nil
+    #   end
+    # end
 
-     def user_signed_in?
-       return true if current_user
-     end
+    # def user_signed_in?
+    #   return true if current_user
+    # end
 
     def correct_user?
       @user = User.find(params[:id])
