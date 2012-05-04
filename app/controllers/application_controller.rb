8f47e5cd996b3deba22c0ab355456863f586e8fa
Eshwar Chandra <eshwarcc@gmail.com>
2012-05-04 15:43:59 +0530

    application controller changed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,19 +3,19 @@ class ApplicationController < ActionController::Base
 
   # helper_method :current_user
   # helper_method :user_signed_in?
-  helper_method :correct_user?
-  #before_filter :set_user_home
+  # helper_method :correct_user?
+  # before_filter :set_user_home
   
 
   private
-    def set_user_home
-    if current_user 
-      @user = User.find(current_user.id)
-      redirect_to @user
-    else
-      redirect_to root_url
-    end
-    end
+    # def set_user_home
+    # if current_user 
+    #   @user = User.find(current_user.id)
+    #   redirect_to @user
+    # else
+    #   redirect_to root_url
+    # end
+    # end
 
     # def current_user
     #   begin
@@ -29,14 +29,14 @@ class ApplicationController < ActionController::Base
     #   return true if current_user
     # end
 
-    def correct_user?
-      @user = User.find(params[:id])
-      unless current_user == @user
-        redirect_to root_url, :alert => "Access denied."
-      end
-    end
+    # def correct_user?
+    #   @user = User.find(params[:id])
+    #   unless current_user == @user
+    #     redirect_to root_url, :alert => "Access denied."
+    #   end
+    # end
     
-    def current_page
-      params[:page] || 1
-    end
+    # def current_page
+    #   params[:page] || 1
+    # end
 end
