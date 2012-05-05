a7c8c18fe5ea8c19c3d2e0ca4150c4da57e323e1
Eshwar Chandra <eshwarcc@gmail.com>
2012-05-06 04:21:37 +0530

    added local facebook api keys

--- config/environment.rb
+++ config/environment.rb
@@ -1,8 +1,11 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
-ENV['FACEBOOK_KEY'] = '282524125170321'
-ENV['FACEBOOK_SECRET'] = '3b63ba8466e9f1a977402f4653231457'
+ENV['FACEBOOK_KEY_HEROKU'] = '282524125170321'
+ENV['FACEBOOK_SECRET_HEROKU'] = '3b63ba8466e9f1a977402f4653231457'
+
+ENV['FACEBOOK_KEY_LOCAL'] = '221969307914563'
+ENV['FACEBOOK_SECRET_LOCAL'] = '0729498050c860339acb88539af074b4'
 
 # Initialize the rails application
 Assure::Application.initialize!
