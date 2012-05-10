f1c9cbc955a4c462ca818a8fec436a01320db344
Venkatesh Tadinada <venkat@solivar.com>
2012-05-10 11:04:16 -0700

    moved fb to surify

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-	provider :facebook, ENV['FACEBOOK_KEY_HEROKU_DEV'], ENV['FACEBOOK_SECRET_HEROKU_DEV'],
+	provider :facebook, ENV['FACEBOOK_KEY_HEROKU_SURIFY'], ENV['FACEBOOK_SECRET_HEROKU_SURIFY'],
+  # provider :facebook, ENV['FACEBOOK_KEY_LOCAL'], ENV['FACEBOOK_SECRET_LOCAL'],
            :scope => 'email,offline_access,read_stream,user_about_me,user_birthday,user_education_history,user_hometown,user_location,user_photos,user_relationships,user_relationship_details,user_work_history,user_website', :display => 'popup'
 end
