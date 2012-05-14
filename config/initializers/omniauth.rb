2eaa4d8d2bf58ddea9e601ebd67c3851dc7f1f13
Eshwar Chandra <eshwarcc@gmail.com>
2012-05-14 23:29:41 +0530

    changed omniauth.rb

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-	provider :facebook, ENV['FACEBOOK_KEY_LOCAL'], ENV['FACEBOOK_SECRET_LOCAL'],
+	provider :facebook, ENV['FACEBOOK_KEY_HEROKU_DEV'], ENV['FACEBOOK_SECRET_HEROKU_DEV'],
            :scope => 'email,offline_access,read_stream,user_about_me,user_birthday,user_education_history,user_hometown,user_location,user_photos,user_relationships,user_relationship_details,user_work_history,user_website', :display => 'popup'
 end
