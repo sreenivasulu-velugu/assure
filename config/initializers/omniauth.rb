2021d3d464e7917b0248a3c566ff6ddad7bd6cdb
Eshwar Chandra <eshwarcc@gmail.com>
2012-05-16 00:02:08 +0530

    Added app_config.yml file to load the different facebook keys depending on the environment

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,8 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-	provider :facebook, ENV['FACEBOOK_KEY_HEROKU_DEV'], ENV['FACEBOOK_SECRET_HEROKU_DEV'],
-           :scope => 'email,offline_access,read_stream,user_about_me,user_birthday,user_education_history,user_hometown,user_location,user_photos,user_relationships,user_relationship_details,user_work_history,user_website', :display => 'popup'
+
+	fb_permissions = 'email,offline_access,read_stream,user_about_me,user_birthday,user_education_history,user_hometown,user_location,user_photos,user_relationships,user_relationship_details,user_work_history,user_website'
+	
+	provider :facebook, APP_CONFIG['facebook_key'], APP_CONFIG['facebook_secret'],
+					 :scope => fb_permissions, :display => 'popup'
+
 end
