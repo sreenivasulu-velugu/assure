3a7300b723be049744ca092ba96f165d65bfba51
Eshwar Chandra <eshwarcc@gmail.com>
2012-05-15 20:47:27 +0530

    Adding the host name for checking purposes

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -3,10 +3,10 @@ class Profile < ActiveRecord::Base
 	# validations
 	# validates :no_of_kids, :numericality => true
 
-  belongs_to :user
-  accepts_nested_attributes_for :user
+	belongs_to :user
+	accepts_nested_attributes_for :user
 
-  def self.relationships
-  	return [['single','Single'], ['committed','Committed'], ['married','Married'], ['complicated','Complicated']]
-  end
+	def self.relationships
+		return [['single','Single'], ['committed','Committed'], ['married','Married'], ['complicated','Complicated']]
+	end
 end
