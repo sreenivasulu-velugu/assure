101074bc24c85c40a0fcf318518d23c9bc60dc7d
Eshwar Chandra <eshwarcc@gmail.com>
2012-05-16 02:07:52 +0530

    1. Eliminate :3 errors prohibited this user from being saved 2. Distribute icons on RHS evenly 3. Fix letter space between "Score" 4. Replace "I am"" with => "My Birthdate is" 5. Remove flash bar

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -11,7 +11,7 @@ class RegistrationsController < Devise::RegistrationsController
 		super
 		if session[:omniauth]
 			@user.apply_omniauth session[:omniauth]
-			@user.valid?
+			# @user.valid?
 		end
 	end
 end
