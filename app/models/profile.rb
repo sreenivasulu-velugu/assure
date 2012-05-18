f6ec58a702edf8fe86b5df08ebf6c5ffbcfe5398
Eshwar Chandra <eshwarcc@gmail.com>
2012-05-18 20:52:11 +0530

    1. UI changes in the nodes bar. 2. functionality changes to get the score.

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -6,4 +6,70 @@ class Profile < ActiveRecord::Base
 	belongs_to :user
 	accepts_nested_attributes_for :user
 
+	def score
+		max_score = 997
+		no_of_items = 18
+		@item_score = max_score/no_of_items
+	end
+
+	def work_score
+		score
+		partial_score = 0
+		(partial_score += @item_score) if self.work.present?
+		(partial_score += @item_score) if self.work_place.present?
+		(partial_score += @item_score) if self.zip_code_or_city.present?
+		return partial_score
+	end
+
+	def birthday_score
+		score
+		partial_score = 0
+		(partial_score += @item_score) if self.work.present?
+		return partial_score
+
+	end
+
+	def relationship_score
+		score
+		partial_score = 0
+		(partial_score += @item_score) if self.relationship_status.present?
+		(partial_score += @item_score) if self.related_to.present?
+		(partial_score += @item_score) if self.no_of_kids.present?
+		return partial_score
+
+	end
+
+	def living_score
+		score
+		partial_score = 0
+		(partial_score += @item_score) if self.house_type.present?
+		(partial_score += @item_score) if self.address.present?
+		(partial_score += @item_score) if self.house_zip_code.present?
+		return partial_score
+
+	end
+
+	def education_score
+		score
+		partial_score = 0
+		(partial_score += @item_score) if self.education.present?
+		(partial_score += @item_score) if self.university.present?
+		(partial_score += @item_score) if self.degree.present?
+		(partial_score += @item_score) if self.area_of_study.present?
+		return partial_score
+	end
+
+	def car_score
+		score
+		partial_score = 0
+		(partial_score += @item_score) if self.car_type.present?
+		(partial_score += @item_score) if self.car_year.present?
+		(partial_score += @item_score) if self.car_make.present?
+		(partial_score += @item_score) if self.car_model.present?
+		return partial_score
+	end
+
+	def total_score
+		self.work_score + self.birthday_score + self.relationship_score + self.living_score + self.education_score + self.car_score
+	end
 end
